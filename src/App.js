b2a430ed67c0fd6caead579f6e3d77f4ec040ccd
Joshua Whitwell <josh.whitwell@gmail.com>
2020-09-08 15:10:02 -0400

    imported data and post and searchBar components to app.js & implemented likePost function on app.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react'
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from '../src/components/Posts/Posts.js'
+import SearchBar from '../src/components/SearchBar/SearchBar.js'
 // Import the dummyData
-import './App.css';
+import dummyData from './dummy-data'
+import './App.css'
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData)
 
   const likePost = postId => {
     /*
-      This function serves the purpose of increasing the number of likes by one, of the post with a given id.
+      This function serves the purpose of increasing the number of likes by one of the post with a given id.
 
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
       This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
@@ -27,14 +31,22 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-  };
+    setPosts(posts.map(element => {
+      if (element.id === postId) {
+        return { ...element, likes: element.likes + 1 }
+      }
+      return element
+    }))
+  }
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts likeFunction={likePost}/>
+      <SearchBar />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
