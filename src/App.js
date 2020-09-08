fbd26436be63e8116f114853730122982230bd20
Joshua Whitwell <josh.whitwell@gmail.com>
2020-09-08 18:56:45 -0400

    added filter method to searchBar.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData)
+  const [inputValue, setValue] = useState('')
 
   const likePost = postId => {
     /*
@@ -40,17 +41,6 @@ const App = () => {
   }
 
   const unlikePost = postId => {
-    /*
-      This function serves the purpose of increasing the number of likes by one of the post with a given id.
-
-      The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
-      This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
-
-      Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
-      The callback passed into `map` performs the following logic:
-        - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
-        - otherwise just return the post object unchanged.
-     */
     setPosts(posts.map(element => {
       if (element.id === postId) {
         return { ...element, likes: element.likes - 1 }
@@ -59,11 +49,18 @@ const App = () => {
     }))
   }
 
+  // const filterPosts = 
+
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
-      <SearchBar />
+      <SearchBar 
+      setValue={setValue}
+      inputValue={inputValue}
+      posts={posts}
+      setPosts={setPosts}
+      />
       <Posts 
       likePost={likePost}
       unlikePost={unlikePost}
