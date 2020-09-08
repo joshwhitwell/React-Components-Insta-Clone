4238ae2f42fec77e8101feed9d764b919ae1ac6e
Joshua Whitwell <josh.whitwell@gmail.com>
2020-09-08 15:35:25 -0400

    added posts ccomponent to posts.js & added post component to post.js

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -10,6 +10,11 @@ const Posts = (props) => {
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {
+        posts.map(element => {
+          return <Post key={element.id} post={element} likePost={likePost}/>
+        })
+      }
     </div>
   );
 };
